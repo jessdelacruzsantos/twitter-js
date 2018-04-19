1f64a0a18a263dae454c4e74e610718a6d6936a5
Jessie De La Cruz Santos <jessdelacruzsantos@gmail.com>
2018-04-19 18:09:15 -0400

    Links working.

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
 
+app.use(morgan('dev'));
+
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', {noCache: true}); 
-
-app.use(function(req, res , next){
-   
-    next(); // Never forget!!!
-});
-
-app.use('/', routes);
+nunjucks.configure('views', {noCache: true});
 
 app.use(express.static('public'));
+app.use(routes);
 
 app.listen(3000, function (){
     console.log('Listening on 3000');
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,25 @@ const router = express.Router();
 
 const tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
     let tweets = tweetBank.list();
-    res.render( 'index.html', {tweets});
-});
 
+    res.render('index', {tweets});
+});
+router.get( '/users/:name', function (req, res, next) {
+    let name = req.params.name;
+    let userTweets = tweetBank.find({name})
+
+    res.render('index', {tweets: userTweets})
+});
+router.get( '/tweets/:id', function (req, res, next) {
+    let id = Number(req.params.id);
+    let userTweet = tweetBank.find({id})
+
+    console.log(userTweet);
+
+    res.render('index', {tweets: userTweet})
+});
 // router.get('/stylesheets/style.css', function (req, res) {
 //     res.sendFile('/Users/jessiedelacruz/Desktop/twitter-js/public/stylesheets/style.css');
 // })
